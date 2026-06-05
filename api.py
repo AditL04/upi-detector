88fad4afa7dbaca3bc5a2b4a7a5befa79eaea10f
AditL04 <157138784+AditL04@users.noreply.github.com>
2026-06-05 12:29:44 +0530

    Update api.py

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://upi-detector-246d.onrender.com"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
